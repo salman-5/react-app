4e46bfffd56d6cf5db2e7a19b2654d0df5eabefa
salman-5 <108650021+salman-5@users.noreply.github.com>
2023-09-02 01:38:19 +0530

    Revert "Changes to Card component"

--- src/components/utils/Card.jsx
+++ src/components/utils/Card.jsx
@@ -1,18 +1,20 @@
-import icon from "../../assets/card.svg";
+import icon from '../../assets/card.svg'
 
 export default function Card(props) {
-  return (
-    <div class="bg-[#252525] w-[380px] rounded-xl p-8 space-y-6">
-      <div class="flex flex-auto space-x-4">
-        <img src={icon} alt="" />
-        <h1 className="text-white font-medium uppercase font-Poppins">
-          {props.title}
-        </h1>
-      </div>
 
-      <p className="text-justify text-white text-s justify font-Open-sans">
-        {props.text}
-      </p>
-    </div>
-  );
+    return (
+
+        <div class='bg-[#252525] w-[380px] rounded-xl p-8 space-y-6'>
+            <div class="flex flex-auto space-x-4">
+                <img src={icon} alt="" />
+                <h1 className='text-white font-medium uppercase font-Poppins'>
+                    {props.title}
+                </h1>
+            </div>
+
+            <p className='text-justify text-white text-s justify font-Open-sans'>{props.text}</p>
+        </div>
+
+
+    );
 }
